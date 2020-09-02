a25f4898737546bf48cae6c58371b9c2c54ac4f4
ghazi noor <ghaziaus@gmail.com>
2020-09-02 18:07:40 +1000

    Initial Changes

---
+++ DatingApp.API/Data/DataContext.cs
@@ -0,0 +1,16 @@
+using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingApp.API.Data
+{
+    public class DataContext : DbContext
+    {
+        
+        public  DataContext(DbContextOptions<DataContext> options) : base(options) {Database.EnsureCreated(); }
+
+        public DbSet<Value> Values { get; set; }
+         
+    
+
+    }
+}
