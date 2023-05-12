ab9438c2ff877f015b9d0913233d558da615309a
Ghazi Mohammed <ghaziaus@gmail.com>
2023-05-12 14:09:32 +1000

    End of section 3

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -9,14 +9,10 @@ namespace API.Controllers
     [Route("api/[controller]")]
     public class UsersController : ControllerBase
     {
-        
         private readonly DataContext _context;
-
         public UsersController(DataContext context)
         {
-            _context = context;
-            
-           
+            _context = context;     
         }
 
         [HttpGet]
@@ -25,12 +21,11 @@ namespace API.Controllers
             var users = await _context.Users.ToListAsync();
             return users;
         }
+
         [HttpGet("{id}")]
-        
         public async Task<ActionResult<AppUser>> GetUser(int id)
         {
            return  await _context.Users.FindAsync(id);
         }
-   
     }
 }
--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<DataContext>(opt =>
 {
    opt.UseSqlite(builder.Configuration.GetConnectionString("Defaultconnection")) ;
 });
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddCors();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 app.Run();
