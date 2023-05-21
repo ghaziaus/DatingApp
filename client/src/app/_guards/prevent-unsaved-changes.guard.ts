e477cab0a35e1ce4287f43eb4504c56cd119749f
Ghazi Mohammed <ghaziaus@gmail.com>
2023-05-22 09:41:10 +1000

    End of section 10

---
+++ client/src/app/_guards/prevent-unsaved-changes.guard.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import { MemberEditComponent } from "../members/member-edit/member-edit.component";
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class  preventUnsavedChangesGuard {
+  canDeactivate(component: MemberEditComponent): boolean {
+    if(component.editForm?.dirty) {
+      return confirm('Are you sure you want to coninue? Any unsaved changes will be lost.')
+    }
+    return true;
+  }
+}
